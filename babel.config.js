4bfc107ea6e4a64270c84c62248990c08ff31db5
thund3rhawk <2025.it18.swadhinpaul@gmail.com>
2025-07-31 22:34:29 +0530

    refactor: test setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    'react-native-reanimated/plugin',
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+    "module:metro-react-native-babel-preset",
   ],
+  plugins: ["react-native-reanimated/plugin"],
 };
