784df2823182d373243627aaa0049bceb1aacca8
thund3rhawk <2025.it18.swadhinpaul@gmail.com>
2025-07-31 22:39:24 +0530

    remove lib

--- lib/components/Accordion/AccordionItem.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { AccordionItemProps } from './types';
-declare const AccordionItem: React.FC<AccordionItemProps>;
-export default AccordionItem;
--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { Accordion, AccordionItem } from './components/Accordion';
-export type { AccordionProps, AccordionItemProps } from './components/Accordion/types';
-export { default as theme } from './theme';
-export type { Theme } from './theme/types';
